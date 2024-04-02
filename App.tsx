f98b7baa4c378f03445d313a694aede4c5c96c25
Aydan Pirani <aydanpirani@gmail.com>
2024-04-01 20:32:20 -0500

    minor routing fixes

--- App.tsx
+++ App.tsx
@@ -10,7 +10,10 @@ export default function App() {
 		<GluestackUIProvider config={config}>
 			<SafeAreaProvider>
 				<NavigationContainer>
-					<Navigation></Navigation>
+				{/* <Text>
+					Hello!
+				</Text> */}
+				<Navigation></Navigation>
 				</NavigationContainer>
 			</SafeAreaProvider>
 		</GluestackUIProvider>
