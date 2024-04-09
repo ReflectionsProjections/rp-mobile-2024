9e0bcda5b8b5ce7a4deca8244c4fe30217902f98
Aryan Bhardwaj <aryan.bhardwaj21@gmail.com>
2024-04-09 18:10:11 -0500

    got basic deep linking functionality working

--- App.tsx
+++ App.tsx
@@ -1,21 +1,60 @@
+import React, { useEffect, useState } from "react";
 import { GluestackUIProvider, Text, Box } from "@gluestack-ui/themed";
 import { config } from "@gluestack-ui/config"; // Optional if you want to use default theme
-import { NavigationContainer } from '@react-navigation/native';
-import Home from "./screens/Home";
+import { NavigationContainer } from "@react-navigation/native";
 import { SafeAreaProvider } from "react-native-safe-area-context";
-import Navigation from "./navigation/Navigation"
+import Navigation from "./navigation/Navigation";
+import * as Linking from "expo-linking";
+import { LinkingEvent } from "expo-linking"; // Import LinkingEvent
+
+const prefix = Linking.createURL("/");
 
 export default function App() {
-	return (
-		<GluestackUIProvider config={config}>
-			<SafeAreaProvider>
-				<NavigationContainer>
-				{/* <Text>
-					Hello!
-				</Text> */}
-				<Navigation></Navigation>
-				</NavigationContainer>
-			</SafeAreaProvider>
-		</GluestackUIProvider>
-	);
+  const [data, setData] = useState(null);
+
+  const linking = {
+	prefixes:[prefix],
+	config:{
+		screens: {
+			Home: "home",
+			Camera: "camera",
+			Events: "events",
+			Profile: "profile",
+			Shop: "shop"
+		}
+	}
+  }
+
+  const handleDeepLink = (event: LinkingEvent) => {
+    let parsedData = Linking.parse(event.url);
+    setData(parsedData);
+  };
+
+  useEffect(() => {
+
+	async function getInitialURL() {
+		const initialURL = await Linking.getInitialURL();
+		if (initialURL) setData(Linking.parse(initialURL));
+	}
+
+    const listener = Linking.addEventListener("url", handleDeepLink);
+	if (!data) {
+		getInitialURL();
+	}
+    return () => listener.remove();
+  }, []);
+
+  return (
+    <GluestackUIProvider config={config}>
+      <SafeAreaProvider>
+        <NavigationContainer linking={linking}>
+          <Navigation></Navigation>
+        </NavigationContainer> 
+      </SafeAreaProvider>
+    </GluestackUIProvider>
+  );
 }
+
+{/* <Box width="100%" height="100%" justifyContent="center" alignItems="center">
+		<Text>{data ? JSON.stringify(data) : "App not opened from link"}</Text>
+		</Box> */}
