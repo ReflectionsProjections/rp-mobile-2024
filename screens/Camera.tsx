9a11cb5c4019441e89b322037ff8bb2621a3d964
caupcakes <78387392+caupcakes@users.noreply.github.com>
2024-04-09 23:23:13 -0500

    add basic camera
    todo - permissions - doing stuff with url

--- screens/Camera.tsx
+++ screens/Camera.tsx
@@ -1,14 +1,42 @@
-import React from "react";
-import { GluestackUIProvider, Text, Box } from "@gluestack-ui/themed";
+import React, {useEffect, useState} from "react";
+import {Box, View, Text} from "@gluestack-ui/themed";
+import {CameraView, useCameraPermissions} from "expo-camera/next";
+import {Linking, StyleSheet, TouchableOpacity} from "react-native";
+import { useIsFocused } from "@react-navigation/native";
+
+
 // import { Button, ButtonText, ButtonIcon, AddIcon } from "@gluestack-ui/themed";
-import { Heading, Center } from "@gluestack-ui/themed"
 
 const Camera: React.FC = () => {
-    return (
-    <Box width="100%" height="100%" justifyContent="center" alignItems="center">
-        <Heading>This is the Camera page.</Heading>
-    </Box>
-    );
-  };
-  
+	const [permission, requestPermission] = useCameraPermissions();
+	const [url, setUrl] = useState("");
+	const isFocused = useIsFocused();
+
+
+	useEffect(() => {
+		requestPermission();
+	});
+
+	if (!permission?.granted) {
+		Linking.openSettings();
+		return <Text>Missing Camera Permissions</Text>;
+	}
+
+	const handleBarCodeScanned = ({ type, data }) => {
+		if (data != url) {
+			alert(`Bar code with type ${type} and data ${data} has been scanned!`);
+			setUrl(data);
+		}
+	};
+
+	return (
+		<Box width="100%" height="100%" justifyContent="center" alignItems="center">
+			{isFocused && <CameraView width="100%" height="100%" facing="back"
+				onBarcodeScanned={handleBarCodeScanned}
+				barcodeScannerSettings={{barcodeTypes: ["qr"]}}
+			/>}
+		</Box>
+	);
+};
+
 export default Camera;
