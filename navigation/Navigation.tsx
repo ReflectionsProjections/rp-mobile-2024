6c95d26902c8a8d3951ed2fb1a4d309b6ea38559
Aryan Bhardwaj <aryan.bhardwaj21@gmail.com>
2024-06-16 13:37:37 -0700

    modified color of navigation bar

--- navigation/Navigation.tsx
+++ navigation/Navigation.tsx
@@ -9,11 +9,12 @@ import Events from "../screens/Events";
 import Camera from "../screens/Camera";
 import Shop from "../screens/Shop";
 import Profile from "../screens/Profile";
+import Colors from "../constants/Colors";
 
 import EventDaysNavigator from "./EventDaysNavigator"; // Adjust path as necessary
 
-const ACTIVE_COLOR = "black";
-const INACTIVE_COLOR = "gray";
+const ACTIVE_COLOR = Colors.YELLOW;
+const INACTIVE_COLOR = Colors.WHITE;
 
 const AppNavigator: React.FC = () => {
   // const Stack = createNativeStackNavigator();
@@ -25,6 +26,9 @@ const AppNavigator: React.FC = () => {
         headerShown: false,
         tabBarActiveTintColor: ACTIVE_COLOR,
         tabBarInactiveTintColor: INACTIVE_COLOR,
+        tabBarStyle: {
+          backgroundColor: Colors.DARK_BLUE,
+        },
         tabBarIcon: ({ focused }) => {
           switch (route.name) {
             case "Home":
@@ -107,21 +111,5 @@ const AppNavigator: React.FC = () => {
     </Tab.Navigator>
   );
 };
-
-// const RootStack = createStackNavigator();
-
-// const Navigation = () => {
-//   return (
-//     <RootStack.Navigator
-//       screenOptions={{
-//         animationEnabled: true,
-//         gestureEnabled: true,
-//         headerShown: false,
-//       }}
-//     >
-//       <RootStack.Screen name="App" component={AppNavigator} />
-//     </RootStack.Navigator>
-//   );
-// };
 
 export default AppNavigator;
