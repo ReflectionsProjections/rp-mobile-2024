df1d83edf4aa870e203073e9d3b9f4ce942928d6
Aryan <abahl3@illinois.edu>
2024-06-23 17:32:40 -0400

    profile page fix

--- screens/Profile.tsx
+++ screens/Profile.tsx
@@ -15,8 +15,34 @@ import { Attendee } from "../redux/types";
 import Colors from "../constants/Colors";
 
 const Profile: React.FC = () => {
+  
+  const dispatch = useAppDispatch();
+  const token = useAppSelector((state: RootState) => state.token);
+  console.log("profile token:", token);
+  const attendee = useAppSelector((state: RootState) => state.attendee);
+  const qrcode = useAppSelector((state: RootState) => state.qrCodeURL);
 
-  const [attendee, setAttendee] = useState<Attendee>(null);
+  useEffect(() => {
+    if (token && !attendee) {
+      dispatch(getAttendee(token));
+    }
+    if (token && !qrcode) {
+      dispatch(getQRCode(token));
+    }
+  }, [token, attendee, qrcode, dispatch]);
+
+  useEffect(() => {
+    const fetchQRCode = async() => {
+      if (token) {
+        await(dispatch(getQRCode(token)));
+      }
+    };
+    fetchQRCode();
+    const interval = setInterval(getQRCode, 20000);
+    return () => clearInterval(interval);
+  }, [token, dispatch]);
+  
+  /* const [attendee, setAttendee] = useState<Attendee>(null);
   const [qrcode, setQRCode] = useState(null);
   const [token, _] = useState(useAppSelector((state: RootState) => state.token));
 
@@ -51,21 +77,17 @@ const Profile: React.FC = () => {
     getQRCode();
     const interval = setInterval(getQRCode, 20000);
     return () => clearInterval(interval);
-  }, [token]);
+  }, [token]); */
 
   return (
-    <Box width="100%" height="100%" paddingVertical={150} flex={1} backgroundColor={Colors.DARK_BLUE}>
+    <Box width="100%" height="100%" paddingVertical={50} flex={1}>
       {attendee && qrcode &&
       <View justifyContent="space-between" alignItems="center">
-        <View style ={{
-          borderWidth: 10, borderColor: Colors.YELLOW, padding: 10, borderRadius: 5,
-        }}>
-          <QRCode
-            value = {qrcode.data}
-            size = {300}
-          />
-        </View>
-        <StyledText variant="bigText" marginTop={50} color={Colors.WHITE}>{attendee.name}</StyledText>
+        <QRCode
+          value = {qrcode}
+          size = {300}
+        />
+        <StyledText variant="bigText" marginTop={50}>{attendee.name}</StyledText>
         <StyledBox variant='foodWave' marginTop={15} justifyContent='center'>
           <StyledText variant="medium">
             {`Food Wave: ${attendee.foodWave}`}
