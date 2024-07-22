fd2beb6e0fbb392231320b228e4a9934b3838595
Aryan <abahl3@illinois.edu>
2024-07-21 21:19:15 -0400

    updated checkin

---
+++ api/postCheckIn.tsx
@@ -0,0 +1,16 @@
+import axios from "axios";
+
+export const postCheckIn = async(token, eventId, qrCode) => {
+    const payload = { eventId, qrCode }
+    try {
+        const response = await axios.post('https://api.reflectionsprojections.org/staff/scan/', payload, {
+            headers: {
+                Authorization: token,
+                'Content-Type': 'application/json'
+            }
+        });
+        console.log('post CheckIn:', response.data)
+    } catch (error) {
+        console.log('Error posting check in:', error)
+    }
+};
