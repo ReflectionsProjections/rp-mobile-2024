ca2a785acf8e7bfdcfb792d250b9f6588bf5492e
Aryan Bhardwaj <aryan.bhardwaj21@gmail.com>
2024-09-12 20:18:25 -0500

    scan merch functionality

--- api/redeemMerch.tsx
+++ api/redeemMerch.tsx
@@ -1,15 +1,22 @@
 import axios from "axios";
 
-export const redeemMerch = async (token: string, merch: string) => {
+export const redeemMerch = async (
+  token: string,
+  merch: string,
+  userID: string
+) => {
+  const payload = { userID };
   try {
     const response = await axios.post(
       `https://api.reflectionsprojections.org/attendee/redeemMerch/${merch}`,
+      payload,
       {
         headers: {
           Authorization: token,
         },
       }
     );
+    console.log(response.data);
     return response.data;
   } catch (error) {
     console.error("Error in redeeming merch:", error);
--- screens/CameraScanner.tsx
+++ screens/CameraScanner.tsx
@@ -1,56 +1,76 @@
-import React, {useEffect, useState} from "react";
+import React, { useEffect, useState } from "react";
 import {
-	Box, Button, ButtonText, ButtonIcon, View, ExternalLinkIcon
+  Box,
+  Button,
+  ButtonText,
+  ButtonIcon,
+  View,
+  ExternalLinkIcon,
 } from "@gluestack-ui/themed";
-import {CameraView, useCameraPermissions} from "expo-camera";
-import {useIsFocused} from "@react-navigation/native";
-import {Linking} from "react-native";
-
+import { CameraView, useCameraPermissions } from "expo-camera";
+import { useIsFocused } from "@react-navigation/native";
+import { Linking } from "react-native";
+import { redeemMerch } from "../api/redeemMerch";
+import { useAppSelector } from "../redux/hooks";
 
 // import { Button, ButtonText, ButtonIcon, AddIcon } from "@gluestack-ui/themed";
 
 const CameraScanner: React.FC = () => {
-	const [status, requestPermission] = useCameraPermissions();
-	const [hasPermission, setHasPermission] = useState(false);
-	const [url, setUrl] = useState("");
-	const isFocused = useIsFocused();
+  const [status, requestPermission] = useCameraPermissions();
+  const [hasPermission, setHasPermission] = useState(false);
+  const [url, setUrl] = useState("");
+  const [scanned, setScanned] = useState(false);
+  const isFocused = useIsFocused();
 
-	useEffect(() => {
-		requestPermission().then((e) => {
-			setHasPermission(e?.granted);
-		});
-	}, []);
+  useEffect(() => {
+    requestPermission().then((e) => {
+      setHasPermission(e?.granted);
+    });
+  }, []);
 
-	const handleBarCodeScanned = ({type, data}) => {
-		if (data != url) {
-			alert(`Bar code with type ${type} and data ${data} has been scanned!`);
-			setUrl(data);
-		}
-	};
+  const user_id = useAppSelector((state: RootState) => state.user_id);
+  const token = useAppSelector((state: RootState) => state.token);
 
-	// mute={true} is used to mute the camera so we dont need mic permissions
-	// https://github.com/expo/expo/issues/27984 of course it is undocumented <333
+  const handleBarCodeScanned = ({ type, data }) => {
+    if (data != url) {
+      alert(`Bar code with type ${type} and data ${data} has been scanned!`);
+      setUrl(data);
+      const response = await redeemMerch(token, data, user_id);
+      setScanned(true);
+    }
+  };
 
-	return (
-		<Box width="100%" height="100%" justifyContent="center" alignItems="center">
-			{isFocused && hasPermission ? (<CameraView mute={true} width="100%" height="100%" facing={"back"}
-				onBarcodeScanned={handleBarCodeScanned}
-				barcodeScannerSettings={{barcodeTypes: ["qr"]}}/>) : (
-				<View>
-					<Button
-						size="md"
-						variant="solid"
-						action="primary"
-						isDisabled={false}
-						isFocusVisible={false}
-						onPress={() => Linking.openSettings()}
-					>
-						<ButtonText>Enable Camera Permission in settings </ButtonText>
-						<ButtonIcon as={ExternalLinkIcon}/>
-					</Button>
-				</View>)}
-		</Box>
-	);
+  // mute={true} is used to mute the camera so we dont need mic permissions
+  // https://github.com/expo/expo/issues/27984 of course it is undocumented <333
+
+  return (
+    <Box width="100%" height="100%" justifyContent="center" alignItems="center">
+      {isFocused && hasPermission ? (
+        <CameraView
+          mute={true}
+          width="100%"
+          height="100%"
+          facing={"back"}
+          onBarcodeScanned={handleBarCodeScanned}
+          barcodeScannerSettings={{ barcodeTypes: ["qr"] }}
+        />
+      ) : (
+        <View>
+          <Button
+            size="md"
+            variant="solid"
+            action="primary"
+            isDisabled={false}
+            isFocusVisible={false}
+            onPress={() => Linking.openSettings()}
+          >
+            <ButtonText>Enable Camera Permission in settings </ButtonText>
+            <ButtonIcon as={ExternalLinkIcon} />
+          </Button>
+        </View>
+      )}
+    </Box>
+  );
 };
 
 export default CameraScanner;
